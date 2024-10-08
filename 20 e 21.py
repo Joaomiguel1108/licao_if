725510a77047ee367e67614bb042db914a37ca2c
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-10-07 21:11:32 -0300

    Update and rename 20 to 20 e 21.py

--- 20 e 21.py
+++ 20 e 21.py
@@ -7,3 +7,7 @@ elif lados == 4:
     print("QUADRADO, com area de", tamanho*lados)
 elif lados == 5:
     print("PENTÁGONO, com area de", tamanho*lados)
+elif lados<3:
+    print("Não é um polígono")
+elif lados>5:
+    print("POLÍGONO NÃO IDENTIFICADO.")
